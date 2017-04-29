f13fdd5aa1e04095189ca707770cf54f5c7d8d08
Bhaumik Panchal <bpanchal@salesforce.com>
2017-04-29 01:43:03 -0400

    chapter 2, challenge 2 - Add a previous button

--- GeoQuizChallenge/app/src/main/java/com/geoquiz/bpanchal/geoquiz/QuizActivity.java
+++ GeoQuizChallenge/app/src/main/java/com/geoquiz/bpanchal/geoquiz/QuizActivity.java
@@ -12,6 +12,7 @@ public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mPrevButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -54,6 +55,16 @@ public class QuizActivity extends AppCompatActivity {
             @Override   
             public void onClick(View v) {
                 checkAnswer(false);
+            }
+        });
+
+        mPrevButton = (Button) findViewById(R.id.previous_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                mCurrentIndex = (mCurrentIndex >= 0) ? mCurrentIndex : mCurrentIndex + mQuestionBank.length;
+                updateQuestion();
             }
         });
 
