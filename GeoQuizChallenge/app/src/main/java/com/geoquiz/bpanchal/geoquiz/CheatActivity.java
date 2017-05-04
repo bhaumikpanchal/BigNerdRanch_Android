378894a4f60e3a5e1131d6766decc4ed93ccc393
Bhaumik Panchal <bpanchal@salesforce.com>
2017-05-04 01:54:23 -0400

    Chapter 5, Challenge 1 - Retain cheating result when screen is rotated

--- GeoQuizChallenge/app/src/main/java/com/geoquiz/bpanchal/geoquiz/CheatActivity.java
+++ GeoQuizChallenge/app/src/main/java/com/geoquiz/bpanchal/geoquiz/CheatActivity.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 
 public class CheatActivity extends AppCompatActivity {
 
+    private static final String TAG = "CheatActivity";
+    private static final String KEY_ANSWER_SHOWN = "isAnswerShown";
     private static final String EXTRA_ANSWER_IS_TRUE = "com.geoquiz.bpanchal.geoquiz.answer_is_true";
     private static final String EXTRA_ANSWER_SHOWN = "com.geoquiz.bpanchal.geoquiz.answer_shown";
 
@@ -17,13 +19,28 @@ public class CheatActivity extends AppCompatActivity {
     private TextView mAnswerTextView;
     private Button mShowAnswerButton;
 
+    private boolean mIsAnswerShown = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
 
+        if(savedInstanceState != null) {
+            mIsAnswerShown = savedInstanceState.getBoolean(KEY_ANSWER_SHOWN);
+        }
+
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
+
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
+        if(mIsAnswerShown) {
+            if(mAnswerIsTrue) {
+                mAnswerTextView.setText(R.string.true_button);
+            } else {
+                mAnswerTextView.setText(R.string.false_button);
+            }
+        }
+
         mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,9 +50,17 @@ public class CheatActivity extends AppCompatActivity {
                 } else {
                     mAnswerTextView.setText(R.string.false_button);
                 }
-                setAnswerShownResult(true);
+                mIsAnswerShown = true;
+                setAnswerShownResult(mIsAnswerShown);
             }
         });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putBoolean(KEY_ANSWER_SHOWN, mIsAnswerShown);
+
     }
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
